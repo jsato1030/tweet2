bd61de1ee78b676d9fb715079018d6f23c38196c
jsato1030 <j_sato1030@yahoo.co.jp>
2016-10-24 13:18:24 +0900

    ツイートを編集する画面を作った。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,6 +20,17 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if current_user.id == tweet.user_id
+      tweet.update(tweet_params)
+    end
+  end
+
   private
   def tweet_params
     params.permit(:image, :text)
